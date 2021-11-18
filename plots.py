92033057c8f2a4258c9890a1314a72a8ff077097
V.Ladyhina <valeriia.ladyhina@gmail.com>
2021-11-18 10:52:19 +0100

    Plot def per_base_sequence_quality was improved - has to be checked for bugs.

--- plots.py
+++ plots.py
@@ -61,4 +61,6 @@ def per_base_sequence_quality():
                 count = 4
     plt.xticks(xticks, xlabels, fontsize=5)
     plt.yticks(fontsize=5)
+    plt.title('Per base sequence quality', fontweight='bold', color='darkred', loc='left')
+    plt.suptitle('\n\n\n\n\nQuality scores across all bases (Sanger / Illumina 1.9 encoding)', size=5)
     plot1.figure.savefig("Per_base_quality.png", figsize=(30, 10), dpi=200)
