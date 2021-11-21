6bc790788692be1aeb0b341359f148288b669a56
V.Ladyhina <valeriia.ladyhina@gmail.com>
2021-11-21 10:45:49 +0100

    Lelya check basis

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def reader(fastq):
             stats.gc_counter(seq, n)
             stats.duplicate_counter(seq, n)
             stats.base_content(seq, n)
+            stats.length_of_reads(n)
             # put other functions from stat.py here
             # they need to work with single read
             # vars: title (use for 'per tile quality'), seq (nucleotides), qual (phred33 quality)
@@ -23,10 +24,11 @@ def reader(fastq):
 
 def report_maker():
     plots.per_base_sequence_quality()
-    plots.per_sequence_GC_content()
+    plots.per_sequence_gc_content()
     plots.overrepresented_table(counter)
     plots.dup_plot_maker(counter)
     plots.per_base_sequence_content()
+    plots.reads_length_distribution()
     # add other functions from plots.py here (which create plots, tables for pdf, etc)
 
 
